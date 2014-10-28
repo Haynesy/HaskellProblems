c818b86da93d673e15af03c99cebf2c85c1c634f
Adam Haynes <haynesy.adam@gmail.com>
2014-10-28 17:32:03 +1100

    Question 5 finished

--- Question005.hs
+++ Question005.hs
@@ -1,10 +1,3 @@
--- (*) Reverse a list
-
--- Prelude> myReverse "A man, a plan, a canal, panama!"
--- "!amanap ,lanac a ,nalp a ,nam A"
--- Prelude> myReverse [1,2,3,4]
--- [4,3,2,1]
-
 import Test.Hspec
 
 main :: IO ()
@@ -18,4 +11,8 @@ spec = describe "Function myReverse" $ do
         (myReverse [1..5] :: [Int]) `shouldBe` [5, 4, 3, 2, 1]
 
 myReverse :: [a] -> [a]
-myReverse = undefined
+myReverse [] = []
+myReverse (x:xs) = if null xs 
+                   then [x]
+                   else myReverse xs ++ [x]
+
--- Question006.hs
+++ Question006.hs
@@ -1,8 +1,23 @@
 -- (*) Find out whether a list is a palindrome. A palindrome can be read forward or backward; e.g. (x a m a x).
 
--- *Main> isPalindrome [1,2,3]
--- False
--- *Main> isPalindrome "madamimadam"
--- True
--- *Main> isPalindrome [1,2,4,8,16,8,4,2,1]
--- True
+import Test.Hspec
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = describe "Function isPalindrome" $ do 
+    it "Should handle a non-palindrome array" $
+        isPalindrome ([1, 2, 3] :: [Int]) `shouldBe` False
+    it "Should handle a string" $
+        isPalindrome "madamimadam" `shouldBe` True
+    it "Should handle an integer arrray" $
+        isPalindrome ([1, 2, 4, 8, 16, 8, 4, 2, 1] :: [Int]) `shouldBe` True
+
+
+isPalindrome :: [a] -> Bool
+isPalindrome [] = True
+isPalindrome _ = False
+-- isPalindrome (x : xs) = if (not $ null xs) && x == last xs
+--                         then isPalindrome (init xs)
+--                         else False
